c9d1cff0d47b2acb597f5cca2d05a7b5e76adb38
Jason Bisson <jasonbisson@hotmail.com>
2020-07-31 12:28:25 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Export GCP Predefined roles
+
+This repository contains a simple bash script to export the GCP predefined IAM roles into the temp directory. The goal of this script is to provide method to easily review the permissions in each role for possible custom roles.
+ 
+## Install
+Download the latest gcloud SDK
+https://cloud.google.com/sdk/docs/
+
+## Permission to export roles
+iam.roles.get
+iam.roles.list
+
+## Usage
+
+### Update default export directory /tmp 
+```
+If needed use your favorite editor or IDE to update the default folder location which is /tmp.
+```
+### Run the export script and be patient
+```
+$ export_predefined_roles.sh
+
+```
+## External Documentation
+
+[Understanding IAM roles](https://cloud.google.com/iam/docs/understanding-roles)
+
---
+++ export_predefined_roles.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -x
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+tempdir=/tmp
+
+printf "Exporting predefined roles can take some time since there are over 500+\n"
+
+for x in $(gcloud iam roles list --format 'value(name)')
+do
+    role=$(echo ${x} |sed 's|roles/||g')
+    printf "Exporting role ${x} to $tempdir/${role}.json\n"
+    gcloud iam roles describe ${x} --format json >  $tempdir/${role}.json
+done
